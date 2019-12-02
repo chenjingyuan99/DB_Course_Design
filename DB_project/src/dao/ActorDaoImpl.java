e27ff09d816726b4b9a3216dc9168011f09f18cd
BarryLee <30632149+xiaoye6@users.noreply.github.com>
2019-12-02 20:38:21 +0800

    ActorDaoImpl

---
+++ DB_project/src/dao/ActorDaoImpl.java
@@ -0,0 +1,47 @@
+package dao;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import entity.Actor;
+
+public class ActorDaoImpl extends BaseDaoImpl implements ActorDao {
+
+    @Override
+    public Actor findByID(int actor_id) {
+
+        Session session = getSession();
+        Query query = session.createQuery("from Actor a where a.actorId=?");
+        query.setInteger(0, actor_id);
+        List list = query.list();
+        session.beginTransaction().commit();
+        session.close();
+        return list.size() > 0 ? (Actor) list.get(0) : null;
+    }
+
+    @Override
+    public List<Actor> findAll() {
+
+        Session session = getSession();
+        Query query = session.createQuery("from Actor");
+        List list = (List<Actor>) query.list();
+        session.beginTransaction().commit();
+        session.close();
+        return list;
+    }
+
+    @Override
+    public List<Actor> findByName(String actor_name) {
+
+        Session session = getSession();
+        Query query = session.createQuery("from Actor a where a.actorName=?");
+        query.setString(0, actor_name);
+        List list = (List<Actor>) query.list();
+        session.beginTransaction().commit();
+        session.close();
+        return list;
+    }
+
+}
