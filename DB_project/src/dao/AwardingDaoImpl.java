c382e6e681260925c5bcefbefe09b1aad2aad2a4
BarryLee <30632149+xiaoye6@users.noreply.github.com>
2019-12-02 20:39:41 +0800

    AwardingDaoImpl

---
+++ DB_project/src/dao/AwardingDaoImpl.java
@@ -0,0 +1,47 @@
+package dao;
+
+import java.util.List;
+
+import entity.Awarding;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+
+public class AwardingDaoImpl extends BaseDaoImpl implements AwardingDao {
+
+    @Override
+    public Awarding findByID(int awards_id) {
+
+        Session session = getSession();
+        Query query = session.createQuery("from Awarding a where a.awardsId=?");
+        query.setInteger(0, awards_id);
+        List list = query.list();
+        session.beginTransaction().commit();
+        session.close();
+        return list.size() > 0 ? (Awarding) list.get(0) : null;
+    }
+
+    @Override
+    public List<Awarding> findAll() {
+
+        Session session = getSession();
+        Query query = session.createQuery("from Awarding");
+        List list = (List<Awarding>) query.list();
+        session.beginTransaction().commit();
+        session.close();
+        return list;
+    }
+
+    @Override
+    public List<Awarding> findByName(String awards_name) {
+
+        Session session = getSession();
+        Query query = session.createQuery("from Awarding a where a.awardsName=?");
+        query.setString(0, awards_name);
+        List list = (List<Awarding>) query.list();
+        session.beginTransaction().commit();
+        session.close();
+        return list;
+    }
+
+}
